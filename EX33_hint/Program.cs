6a43917cca20016b8e365d284ee3b5c7d35c1e0d
Rukia <92082584+rukia01@users.noreply.github.com>
2022-03-01 09:52:49 +0900

    Update Program.cs

--- EX33_hint/Program.cs
+++ EX33_hint/Program.cs
@@ -8,7 +8,15 @@ namespace EX33_hint
         {
             RectAngle rectAngle1 = new RectAngle(5, 4);
             RectAngle rectAngle2 = new RectAngle(6, 2);
-            Console.WriteLine($"{rectAngle1 + rectAngle2}");
+            if (rectAngle1 == rectAngle2)
+            {
+                Console.WriteLine("同じ四角形");
+            }
+            else
+            {
+                Console.WriteLine("違う四角形");
+            }
+            Console.WriteLine($"rectAngle1とrectAngle2の二つの四角形が入る四角形の面積は{rectAngle1 + rectAngle2}");
         }
     }
 }
