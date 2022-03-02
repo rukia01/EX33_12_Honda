8a26dbb72f3b6d535af53ae4e27fe8218c13aca2
Rukia <92082584+rukia01@users.noreply.github.com>
2022-03-02 09:35:48 +0900

    Update Program.cs

--- EX33_hint/Program.cs
+++ EX33_hint/Program.cs
@@ -6,7 +6,7 @@ namespace EX33_hint
     {
         static void Main(string[] args)
         {
-            RectAngle rectAngle1 = new RectAngle(10, 8);
+            RectAngle rectAngle1 = new RectAngle(8, 10);
             RectAngle rectAngle2 = new RectAngle(3, 2);
             var rectAngle3 = rectAngle1 + rectAngle2;
             if (rectAngle1 == rectAngle2)
